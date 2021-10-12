5f0aacf82a9f3d80e66e19cb9510be33c564e85b
unknown <maximilian.gerhardt@rub.de>
2021-10-12 09:40:08 +0200

    Implement and verify device filter

--- misc/scripts/gd32_genpinmap.py
+++ misc/scripts/gd32_genpinmap.py
@@ -134,7 +134,7 @@ class GD32AdditionalFuncFamiliy:
         self.additional_funcs = additional_funcs
 
 class GD32AlternateFunc:
-    def __init__(self, signal_name:str, af_number:int, footnote:str, footnote_device_availability:Dict[str,str]) -> None:
+    def __init__(self, signal_name:str, af_number:int, footnote:str, footnote_device_availability:Dict[str,List[str]]) -> None:
         self.signal_name = signal_name
         self.footnote = footnote
         self.footnote_device_availability = footnote_device_availability
@@ -184,7 +184,42 @@ class GD32PinMap:
         results_ad = self.search_pins_for_add_func(criteria_type, criteria_value)
         return results_af + results_ad
 
-    def search_pins_for_add_func(self, criteria_type, criteria_value) -> List[Tuple[GD32Pin, GD32AdditionalFunc]]:
+    # Examples: devicename = GS32F190T6, constraint = GD32F190T8 => false
+    #           devicename = GS32F190T6, constraint = GD32F190T6 => true
+    #           devicename = GS32F190T6, constraint = GD32F190Rx => false
+    #           devicename = GS32F190T6, constraint = GD32F190Tx => true
+    @staticmethod
+    def devicename_matches_constraint(device_name:str, constraint:str) -> bool:
+        if "x" not in constraint:
+            ret = device_name == constraint
+        else: 
+            # replace x with "can be any character" regex
+            regex_str = constraint.replace("x", ".")
+            ret = re.match(regex_str, device_name) != None
+        print("Checked constraint \"%s\" against devicename \"%s\" -> %s" % (constraint, device_name, str(ret)))
+        return ret
+
+    @staticmethod
+    def does_af_pass_filter(af: GD32AlternateFunc, device_name:str) -> bool:
+        # we can't decide without a devicename, so we'll let anyhting pass
+        if device_name is None: 
+            return True
+        # no device constraint per footnote? Then this passes.
+        if af.footnote_resolved == None:
+            return True
+        else:
+            return any([GD32PinMap.devicename_matches_constraint(device_name, dev_constraint) for dev_constraint in af.footnote_resolved])
+
+    @staticmethod
+    def does_ad_pass_filter(ad: GD32AdditionalFunc, device_name:str) -> bool:
+        # we can't decide without a devicename, so we'll let anyhting pass
+        if device_name is None: 
+            return True
+        # all additional functions were saved with their sub-series.
+        else:
+            return GD32PinMap.devicename_matches_constraint(device_name, ad.subseries)
+
+    def search_pins_for_add_func(self, criteria_type, criteria_value, filter_device_name:str=None) -> List[Tuple[GD32Pin, GD32AdditionalFunc]]:
         results = list()
         for p in self.pin_map.values():
             # search through all additional functions
@@ -198,9 +233,10 @@ class GD32PinMap:
                 elif criteria_type == GD32PinMap.CRITERIA_PERIPHAL_AND_PIN_SUB_FUNCTION:
                     if additional_func.peripheral.startswith(criteria_value[0]) and (additional_func.subfunction is not None and criteria_value[1] in additional_func.subfunction):
                         results.append((p, additional_func))
+        results = list(filter(lambda p_func_tuple: GD32PinMap.does_ad_pass_filter(p_func_tuple[1], filter_device_name), results))
         return results
 
-    def search_pins_for_af(self, criteria_type, criteria_value) -> List[Tuple[GD32Pin, GD32AlternateFunc]]:
+    def search_pins_for_af(self, criteria_type, criteria_value,filter_device_name:str=None) -> List[Tuple[GD32Pin, GD32AlternateFunc]]:
         results = list()
         for p in self.pin_map.values():
             # search through all alternate functions pins
@@ -215,6 +251,7 @@ class GD32PinMap:
                     elif criteria_type == GD32PinMap.CRITERIA_PERIPHAL_AND_PIN_SUB_FUNCTION:
                         if alternate_func.peripheral.startswith(criteria_value[0]) and (alternate_func.subfunction is not None and criteria_value[1] in alternate_func.subfunction):
                             results.append((p, alternate_func))
+        results = list(filter(lambda p_func_tuple: GD32PinMap.does_af_pass_filter(p_func_tuple[1], filter_device_name), results))
         return results
         
 class GD32PinMapGenerator:
@@ -243,6 +280,19 @@ class GD32PinMapGenerator:
         all_adc_pins = pinmap.search_pins_for_add_func(GD32PinMap.CRITERIA_PERIPHERAL_STARTS_WITH, "ADC")
         for pin, func in all_adc_pins:
             print("Found ADC pin %s (func %s, periph %s, subseries %s)" % (pin.pin_name, func.signal_name, func.peripheral, func.subseries))  
+
+        print("Testing search for only a device name.")
+        all_adc_pins = pinmap.search_pins_for_add_func(GD32PinMap.CRITERIA_PERIPHERAL_STARTS_WITH, "ADC", filter_device_name="GD32F190T6")
+        for pin, func in all_adc_pins:
+            print("Found ADC pin %s (func %s, periph %s, subseries %s)" % (pin.pin_name, func.signal_name, func.peripheral, func.subseries))  
+
+        all_i2c_sda_pins = pinmap.search_pins_for_af(GD32PinMap.CRITERIA_PIN_SUB_FUNCTION, "SDA", filter_device_name="GD32F190T6")
+        for pin, func in all_i2c_sda_pins:
+            print("Found I2C SDA pin %s (AF%d, func %s, periph %s, footnote %s %s)" % (pin.pin_name, func.af_number, func.signal_name, func.peripheral, func.footnote, func.footnote_resolved))  
+
+        all_i2c_sda_pins = pinmap.search_pins_for_af(GD32PinMap.CRITERIA_PIN_SUB_FUNCTION, "SDA", filter_device_name="GD32F190T4")
+        for pin, func in all_i2c_sda_pins:
+            print("Found I2C SDA pin %s (AF%d, func %s, periph %s, footnote %s %s)" % (pin.pin_name, func.af_number, func.signal_name, func.peripheral, func.footnote, func.footnote_resolved))  
 
         #print(pinmap.pin_map["PB7"])
         pass
