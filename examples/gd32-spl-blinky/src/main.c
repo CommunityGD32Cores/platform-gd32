93c938f4abdd84991c39c1b342e89984846f44cc
unknown <maximilian.gerhardt@rub.de>
2021-12-19 18:44:34 +0100

    Add GD32W51x to CI

--- examples/gd32-spl-blinky/src/main.c
+++ examples/gd32-spl-blinky/src/main.c
@@ -20,6 +20,8 @@
 #include "gd32e50x.h"
 #elif defined(GD32L23x)
 #include "gd32l23x.h"
+#elif defined(GD32W51x)
+#include "gd32w51x.h"
 #else
 #error "Unknown chip series"
 #endif
@@ -46,9 +48,9 @@ int main(void)
     rcu_periph_clock_enable(LED_CLOCK);
 
     /* set output as output */
-#if defined(GD32F3x0) || defined(GD32F1x0) || defined(GD32F4xx) || defined(GD32E23x) || defined(GD32L23x)
+#if defined(GD32F3x0) || defined(GD32F1x0) || defined(GD32F4xx) || defined(GD32E23x) || defined(GD32L23x) || defined(GD32W51x)
     gpio_mode_set(LEDPORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDPIN);
-    gpio_output_options_set(LEDPORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, LEDPIN);
+    gpio_output_options_set(LEDPORT, GPIO_OTYPE_PP, GPIO_OSPEED_10MHZ, LEDPIN);
 #else /* valid for GD32F10x, GD32E20x, GD32F30x, GD32F403, GD32E10X */
     gpio_init(LEDPORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, LEDPIN);
 #endif
